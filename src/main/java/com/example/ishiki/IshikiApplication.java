a1c014546f55fa48bedc55b9daf7ac255cc00df3
mourchid <mourchid.moutuidine@hotmail.com>
2024-10-08 15:19:30 +0200

    create project

---
+++ src/main/java/com/example/ishiki/IshikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ishiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IshikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ishiki/IshikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ishiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IshikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
